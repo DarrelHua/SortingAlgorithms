1401839766b7ffe8d34c09499a46dc314ca5b861
Darrel Hua <darrelhua@Outlook.com>
2021-05-18 15:13:04 -0700

    Made changes in main to test mergeSort

--- main.cpp
+++ main.cpp
@@ -1,6 +1,17 @@
 #include <iostream>
 using namespace std;
 #include "quickSort.cpp"
+#include "mergeSort.cpp"
+
+
+// UTILITY FUNCTIONS
+// Function to print an array
+void printArray(int A[], int size)
+ {
+    for (int i = 0; i < size; i++)
+        cout << A[i] << " ";
+ }
+
 
 int main() {
     int sz;
@@ -8,22 +19,20 @@ int main() {
    cin>>sz;
    int randArray[sz];
    cout << "Before Sorting" << endl;
+
    for(int i=0;i<sz;i++) {
       randArray[i]=rand()%100;  //Generate number between 0 to 99
       cout << randArray[i] << " ";
    }
   
-   quickSort(randArray, 0, sizeof(randArray));
+   //quickSort(randArray, 0, sizeof(randArray));
+   mergeSort(randArray, 0 , sz-1);
+
    cout << endl;
    cout << "Sorted Array: " << endl;
-   printArray(randArray, sizeof(randArray));
+   printArray(randArray, sz);
    return 0;
+
+
 }
 
-// UTILITY FUNCTIONS
-// Function to print an array
-void printArray(int A[], int size)
-{
-    for (int i = 0; i < size; i++)
-        cout << A[i] << " ";
-}
