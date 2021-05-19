aa7a76096740b98aeeaf9518fe22d48c2ec6880d
Darrel Hua <darrelhua@outlook.com>
2021-05-19 08:10:55 -0700

    Testing git connection

--- main.cpp
+++ main.cpp
@@ -1,57 +1,61 @@
 #include <iostream>
+#include <iomanip>
 using namespace std;
+
 #include "quickSort.cpp"
 #include "mergeSort.cpp"
 #include "selectionSort.cpp"
 #include "bubbleSort.cpp"
 
-
 // UTILITY FUNCTIONS
 // Function to print an array
 void printArray(int A[], int size)
- {
+{
     for (int i = 0; i < size; i++)
         cout << A[i] << " ";
- }
+}
 
-
-int main() {
+int main()
+{
     int sz;
-   cout<<"Enter the size of array::";
-   cin>>sz;
-   int randArray[sz];
-   cout << "Before Sorting" << endl;
+    cout << "Enter the size of array::";
+    cin >> sz;
+    int randArray[sz];
+    cout << "Before Sorting" << endl;
 
-   for(int i=0;i<sz;i++) {
-      randArray[i]=rand()%100;  //Generate number between 0 to 99
-      cout << randArray[i] << " ";
-   }
+    for (int i = 0; i < sz; i++)
+    {
+        randArray[i] = rand() % 100; //Generate number between 0 to 99
+    }
 
-   int selection;
-   cout << endl << "Select Sorting Algorithm:" << endl;
-   cout << "1) Quick Sort" << endl;
-   cout << "2) Merge Sort" << endl;
-   cout << "3) Selection Sort" << endl;
-   cout << "4) Bubble Sort" << endl;
+    int selection;
+    cout << endl;
 
-   cout << "Algorithm #: ";
-   cin >> selection;
-   cout << endl;
+    cout << endl
+         << "Select Sorting Algorithm:" << endl;
+    cout << setw(6) << "1) Quick Sort" << endl;
+    cout << setw(6) << "2) Merge Sort" << endl;
+    cout << setw(6) << "3) Selection Sort" << endl;
+    cout << setw(6) << "4) Bubble Sort" << endl;
+    cout << endl;
+
+    cout << "Algorithm #: ";
+    cin >> selection;
+    cout << endl;
+
+    time_t start, end;
 
     if (selection == 1)
         quickSort(randArray, 0, sizeof(randArray));
     else if (selection == 2)
-        mergeSort(randArray, 0 , sz-1);
-    else if (selection ==3)
+        mergeSort(randArray, 0, sz - 1);
+    else if (selection == 3)
         selectionSort(randArray, sz);
-    else if (selection ==4)
+    else if (selection == 4)
         bubbleSort(randArray, sz);
 
-   cout << endl;
-   cout << "Sorted Array: " << endl;
-   printArray(randArray, sz);
-   return 0;
-
-
+    cout << endl;
+    cout << "Sorted Array: " << endl;
+    
+    return 0;
 }
-
