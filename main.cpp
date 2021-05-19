4d96693e7869681b9a3a65acb39f5003d43ab672
Darrel Hua <darrelhua@outlook.com>
2021-05-19 08:43:46 -0700

    Changed to print execution time

--- main.cpp
+++ main.cpp
@@ -1,19 +1,12 @@
 #include <iostream>
 #include <iomanip>
+// #include <algorithm>
 using namespace std;
 
 #include "quickSort.cpp"
 #include "mergeSort.cpp"
 #include "selectionSort.cpp"
 #include "bubbleSort.cpp"
-
-// UTILITY FUNCTIONS
-// Function to print an array
-void printArray(int A[], int size)
-{
-    for (int i = 0; i < size; i++)
-        cout << A[i] << " ";
-}
 
 int main()
 {
@@ -37,6 +30,7 @@ int main()
     cout << setw(6) << "2) Merge Sort" << endl;
     cout << setw(6) << "3) Selection Sort" << endl;
     cout << setw(6) << "4) Bubble Sort" << endl;
+    cout << setw(6) << "5) Comnpare All" << endl;
     cout << endl;
 
     cout << "Algorithm #: ";
@@ -44,18 +38,69 @@ int main()
     cout << endl;
 
     time_t start, end;
+    double total;
+    if (selection == 1) {
+        time(&start);
+        quickSort(randArray,0,sz);
+        time(&end);
+        double total = double(end-start);
+        cout << "Quick Sort took " << total << setprecision(3) << endl;
+    }
+    else if (selection == 2){
+        time(&start);
+        mergeSort(randArray,0,sz-1);
+        time(&end);
+        total = double(end-start);
+        cout << "Merge Sort took " << total << setprecision(3) << endl;
+    }
+    else if (selection == 3) {
+        time(&start);
+        selectionSort(randArray,sz);
+        time(&end);
+        double total = double(end-start);
+        cout << "Selection Sort took " << total << setprecision(3) << endl;
+    }
+    else if (selection == 4) {
+        time(&start);
+        bubbleSort(randArray,sz);
+        time(&end);
+        double total = double(end-start);
+        cout << "Bubble Sort took " << total << setprecision(3) << endl;
+    }
+    else if (selection == 5) {
+        int arr1[sz];
+        copy(randArray[0],randArray[sz-1], arr1[0]);
+        time(&start);
+        quickSort(arr1,0,sz);
+        time(&end);
+        total = double(end-start);
+        cout << "Quick Sort took " << total << setprecision(3) << endl;
 
-    if (selection == 1)
-        quickSort(randArray, 0, sizeof(randArray));
-    else if (selection == 2)
-        mergeSort(randArray, 0, sz - 1);
-    else if (selection == 3)
-        selectionSort(randArray, sz);
-    else if (selection == 4)
-        bubbleSort(randArray, sz);
+        int arr2[sz];
+        copy(randArray[0],randArray[sz-1], arr2[0]);
+        time(&start);
+        mergeSort(arr1,0,sz-1);
+        time(&end);
+        total = double(end-start);
+        cout << "Merge Sort took " << total << setprecision(3) << endl;
 
-    cout << endl;
-    cout << "Sorted Array: " << endl;
+        int arr3[sz];
+        copy(randArray[0],randArray[sz-1], arr3[0]);
+        time(&start);
+        selectionSort(arr1,sz);
+        time(&end);
+        total = double(end-start);
+        cout << "Selection Sort took " << total << setprecision(3) << endl;
+
+        int arr4[sz];
+        copy(randArray[0],randArray[sz-1], arr4[0]);
+        time(&start);
+        bubbleSort(arr1,sz);
+        time(&end);
+        total = double(end-start);
+        cout << "Bubble Sort took " << total << setprecision(3) << endl;
+    }
+    
     
     return 0;
 }
